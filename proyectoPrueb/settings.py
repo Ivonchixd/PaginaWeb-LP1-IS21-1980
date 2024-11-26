5715b9f9a38f43df1bbd881221aff53b5c4182e2
Ivonchixd <is21-1980@unphu.edu.do>
2024-11-26 18:26:16 -0400

    Avancs 2

--- proyectoPrueb/settings.py
+++ proyectoPrueb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^y9quw8@)q45u80*hu)*nl0ile4_90)vfvrj1l^v_iqy=0l8)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
